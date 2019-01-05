73fcd40e4edd587c1e26784646ca97525d620c4d
Dylan Garcia <dag82358@uga.edu>
2019-01-05 10:02:51 -0500

    Update types for Query String

--- src/queryString.ts
+++ src/queryString.ts
@@ -1,26 +1,30 @@
+export interface QueryString {
+  [key: string]: string | number | boolean;
+}
+
 const queryString = {
-  fromString(str) {
+  fromString(str: string): QueryString {
     if (!str) {
       return {};
     }
 
-    if (str[0] === "?") {
+    if (str[0] === '?') {
       str = str.substring(1);
     }
 
     return str
-      .split("&")
-      .map(kv => kv.split("="))
+      .split('&')
+      .map(kv => kv.split('='))
       .reduce((acc, [k, v]) => {
         acc[k] = v;
         return acc;
       }, {});
   },
 
-  toString(obj) {
+  toString(obj: QueryString) {
     return Object.keys(obj)
       .map(key => `${key}=${obj[key]}`)
-      .join("&");
+      .join('&');
   }
 };
 
